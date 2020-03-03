ac092b7fef9f8607a9469f147f18b0472da0374e
BalaajiChander <balajichander.r@gmail.com>
2020-03-03 10:42:33 +0530

    Added Recipes.

--- src/main/java/com/bala/java/jee/jax_rs/application/recipe/boundary/RecipeEndPoint.java
+++ src/main/java/com/bala/java/jee/jax_rs/application/recipe/boundary/RecipeEndPoint.java
@@ -1,10 +1,11 @@
 package com.bala.java.jee.jax_rs.application.recipe.boundary;
 
+import com.bala.java.jee.jax_rs.application.recipe.controller.RecipeController;
+import com.bala.java.jee.jax_rs.application.recipe.entity.Recipe;
+
+import javax.inject.Inject;
 import javax.json.Json;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -13,15 +14,75 @@ import javax.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON)
 public class RecipeEndPoint {
 
+    @Inject
+    RecipeController recipeController;
+
     /**
      * Auto Generated default Constructor stub
      */
     public RecipeEndPoint() {
     }
 
+    /**
+     * @return
+     */
     @GET
     @Path("/test")
     public Response test() {
+        return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
+    }
+
+    /**
+     * @return
+     */
+    @GET
+    public Response getAllRecipes() {
+        return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @GET
+    @Path("/{id}}")
+    public Response getRecipe(@PathParam("id") final String id) {
+        return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
+    }
+
+    /**
+     * @param recipe
+     * @return
+     */
+    @POST
+    public Response createRecipe(Recipe recipe) {
+        return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
+    }
+
+    /**
+     * @param recipe
+     * @return
+     */
+    @PUT
+    public Response updateRecipe(Recipe recipe) {
+        return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
+    }
+
+    /**
+     * @return
+     */
+    @PATCH
+    public Response patchRecipe() {
+        return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @DELETE
+    @Path("/{id}}")
+    public Response deleteRecipe(@PathParam("id") final String id) {
         return Response.ok().entity(Json.createObjectBuilder().add("isWorking", "Yes!! As Expected").build()).build();
     }
 
---
+++ src/main/java/com/bala/java/jee/jax_rs/application/recipe/entity/Recipe.java
@@ -0,0 +1,9 @@
+package com.bala.java.jee.jax_rs.application.recipe.entity;
+
+public class Recipe {
+    /**
+     * Auto generated default constructor stub
+     */
+    public Recipe() {
+    }
+}
